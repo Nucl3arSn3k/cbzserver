5abaf087a523d8e03bca78d2a83e2f4aa9acb6c8
Nucl3arSn3k <shredderofwheat@gmail.com>
2025-01-16 11:00:00 -0500

    Mint SPA
    initalizing basic mint framework

--- dist/pkg/perseus_engine_bg.wasm.d.ts
+++ dist/pkg/perseus_engine_bg.wasm.d.ts
@@ -2,19 +2,17 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const main: (a: number, b: number) => number;
-export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_exn_store: (a: number) => void;
-export const __externref_table_alloc: () => number;
 export const __wbindgen_free: (a: number, b: number, c: number) => void;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const __wbindgen_export_6: WebAssembly.Table;
-export const closure214_externref_shim: (a: number, b: number, c: any) => void;
-export const closure218_externref_shim: (a: number, b: number, c: any) => void;
-export const _dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he809008748cdd2b5: (a: number, b: number) => void;
-export const closure216_externref_shim: (a: number, b: number, c: any) => void;
-export const closure382_externref_shim_multivalue_shim: (a: number, b: number, c: any) => [number, number];
-export const __externref_table_dealloc: (a: number) => void;
-export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h30860bf716998853: (a: number, b: number) => void;
-export const closure438_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0fe668be1fa1e429: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h62fbd9da1937132a: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8c57886a227e4a1c: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he7af4d8fec5b2457: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha8d2fc8736b41f00: (a: number, b: number, c: number, d: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc770e93cdc6c45b9: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h446bd8bf0f8da276: (a: number, b: number, c: number) => void;
 export const __wbindgen_start: () => void;
